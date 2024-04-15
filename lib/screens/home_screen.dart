c97120a9302e8961932b56e8a2c89d773f81611d
Lans12345 <olanalans12345@gmail.com>
2024-04-15 19:35:46 +0800

    added color

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -110,7 +110,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         onTap: () {
                           Navigator.of(context).push(MaterialPageRoute(
                               builder: (context) => ARView(
-                                    file: 'shapeTriangle2',
+                                    file: 'colorRed',
                                   )));
                         },
                         child: Image.asset(
