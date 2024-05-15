a30a36c9f0e8f1af2f7ec4ad39d8d7bd0291374e
Lans12345 <olanalans12345@gmail.com>
2024-05-16 06:16:24 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:solar_mobile/firebase_options.dart';
 import 'package:solar_mobile/screens/landing_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'learn-7123f',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
